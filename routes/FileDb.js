1bce5da2f70f6a4280e2107d8bdfb05a2868f6d0
cgp <chenguopeng@linctex.com>
2024-11-05 10:08:34 +0800

    练习2: 增删改查

---
+++ routes/FileDb.js
@@ -0,0 +1,53 @@
+const fs=require('fs')
+
+class FileDb {
+    add(info){
+        try{
+            let data=JSON.parse(fs.readFileSync('../file.txt',"utf-8"))
+            console.log('read data', data);
+            data.push(info)
+            data = JSON.stringify(data)
+            fs.writeFileSync('../file.txt',data,"utf-8");
+            console.log("add",data);
+            return data
+        }catch(err){
+            console.log(err)
+        }
+    }
+    delete(name){
+        try{
+            let data=JSON.parse(fs.readFileSync('../file.txt',"utf-8"))
+            const index=data.findIndex(i=>i.name===name)
+            data.splice(index,1)
+            data = JSON.stringify(data)
+            fs.writeFileSync('../file.txt',data,"utf-8")
+            console.log("delete",data);
+            return data
+        }catch(err){
+            console.log(err)
+        }
+    }
+    edit(){
+        try{
+            let data=JSON.parse(fs.readFileSync('../file.txt',"utf-8"))
+            data.map(i=>i.score+=1)
+            data = JSON.stringify(data)
+            fs.writeFileSync('../file.txt',data,"utf-8")
+            console.log("edit",data);
+            return data
+        }catch(err){
+            console.log(err)
+        }
+    }
+    query(name){
+        try{
+            const data=JSON.parse(fs.readFileSync('../file.txt',"utf-8"))
+            const userInfo=data.find(i=>i.name===name)
+            console.log("query",userInfo);
+            return userInfo
+        }catch(err){
+            console.log(err)
+        }
+    }
+}
+module.exports = FileDb;
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,41 @@
+var FileDb = require('./FileDb')
 var express = require('express');
 var router = express.Router();
-
-const fs=require('fs')
-// let userList = [{ name: 'cgp', age: 18 , score: 70}, {name: 'yeye', age: 18 , score: 100}];
-
-const write=(userList)=>{
-  try{
-      const data = JSON.stringify(userList)
-      fs.writeFileSync('../file.txt',data,"utf-8")
-  }catch(err){
-      console.log(err)
-  }
-}
-const read=()=>{
-  try{
-      const data=JSON.parse(fs.readFileSync('../file.txt',"utf-8"))
-      return data
-  }catch(err){
-      console.log(err)
-  }
-}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Welecome to school');
 });
 
-router.get('/user', function(req, res, next) {
-  const userList = read();
-  res.send(userList);
-  write(userList)
-})
-
+// 增
 router.get('/user/add', function(req, res, next) {
-  const userList = read();
-  userList.map(i => i.score += 1);
-  write(userList)
+  var file=new FileDb() 
+  const userList = file.add({"name":"cgp","age":18,"score":70});
+  console.log(userList);
+  res.send("OK");
+})
+// 删
+router.get('/user/delete', function(req, res, next) {
+  var file=new FileDb() 
+  const userList = file.delete("cgp");
+  // res.send(userList);
+  console.log(userList);
+  res.send("OK");
+})
+// 改
+router.get('/user/edit', function(req, res, next) {
+  var file=new FileDb() 
+  const userList = file.edit();
+  // res.send(userList);
+  console.log(userList);
+  res.send("OK");
+})
+// 查
+router.get('/user/query', function(req, res, next) {
+  var file=new FileDb() 
+  const userList = file.query("cgp");
+  console.log(userList);
   res.send(userList);
 })
 
-router.get('/user/reduce', function(req, res, next) {
-  const userList = read();
-  userList.map(i => i.score -= 1);
-  write(userList)
-  res.send(userList);
-})
-
-
-// read()
-// write()
 module.exports = router;
